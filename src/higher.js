47edb1aaf7ab31ec1428322ec727cd85ef8753b5
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-24 17:54:57 +0300

    Fewer utility functions in global scope

--- src/higher.js
+++ src/higher.js
@@ -1,44 +1,59 @@
-const getPrototypeWrapperFunction = function(name, expected, implementation){
-    if(expectSingularArgument(expected.sequences)){
-        const numbers = expectArgumentCountType(expected.numbers);
-        const functions = expectArgumentCountType(expected.functions);
-        if(numbers === 0 && functions === 0){
-            return function(){
-                return implementation(this);
-            };
-        }else if(numbers === 0 || functions === 0){
-            const validate = function(args){
-                const found = countArgumentTypes(args);
-                const error = compareExpectedArguments(
-                    expected, found.numbers, found.functions,
-                    1 + found.sequences, found.invalid
-                );
-                if(error) throw (
-                    `Error validating arguments for function "${name}": ${error}`
-                );
-            };
-            if(numbers === 1 || functions === 1){
-                return function(){
-                    validate(arguments);
-                    return implementation(arguments[0], this);
-                };
-            }else{
-                return function(){
-                    validate(arguments);
-                    return implementation(arguments, this);
-                };
-            }
+const hi = {
+    version: "0.1.0",
+    
+    internal: {
+        // Gets filled with utilities for creating function wrappers.
+        getWrapper: {},
+    },
+    
+    isSequence: isSequence,
+    asSequence: asSequence,
+    seq: asSequence,
+    
+    registeredFunctions: [],
+    register: function(name, expected, implementation){
+        let register = {
+            name: name,
+            expected: expected,
+            implementation: implementation,
+            fancy: this.internal.getWrapper.fancy(name, expected, implementation),
+        };
+        this[name] = register.fancy;
+        if(!expectNoArgument(expected.sequences)){
+            prototypeMethod = this.internal.getWrapper.sequenceMethod(
+                name, expected, implementation
+            );
+            Sequence.prototype[name] = prototypeMethod;
+            register.prototypeMethod = prototypeMethod;
         }
-    }
-    return function(){
-        Array.prototype.splice.call(arguments, 0, 0, this)
-        return validateArguments(expected, arguments, implementation, function(error){
-            throw `Error validating arguments for function "${name}": ${error}`;
-        });
-    };
+        if(expected.async){
+            const fancyAsync = this.internal.getWrapper.async(
+                (caller, args) => fancy.apply(caller, args)
+            );
+            this[name + "Async"] = fancyAsync;
+            register.fancyAsync = fancyAsync;
+        }
+        if(expected.async && register.prototypeMethod){
+            const protoAsync = this.internal.getWrapper.async(
+                (caller, args) => prototypeMethod.apply(caller, args)
+            );
+            Sequence.prototype[name + "Async"] = protoAsync;
+            register.prototypeMethodAsync = protoAsync;
+        }
+        this.registeredFunctions.push(register);
+        return register.fancy;
+    },
+    alias: function(name, alias){
+        if(name in this){
+            this[alias] = this[name];
+        }
+        if(name + "Async" in this){
+            this[alias + "Async"] = this[name + "Async"];
+        }
+    },
 };
 
-const getFancyWrapperFunction = function(name, expected, implementation){
+hi.internal.getWrapper.fancy = function(name, expected, implementation){
     const numbers = expectArgumentCountType(expected.numbers);
     const functions = expectArgumentCountType(expected.functions);
     const sequences = expectArgumentCountType(expected.sequences);
@@ -90,63 +105,53 @@ const getFancyWrapperFunction = function(name, expected, implementation){
     };
 };
 
-const getAsyncWrapperFunction = function(callback){
+hi.internal.getWrapper.sequenceMethod = function(name, expected, implementation){
+    if(expectSingularArgument(expected.sequences)){
+        const numbers = expectArgumentCountType(expected.numbers);
+        const functions = expectArgumentCountType(expected.functions);
+        if(numbers === 0 && functions === 0){
+            return function(){
+                return implementation(this);
+            };
+        }else if(numbers === 0 || functions === 0){
+            const validate = function(args){
+                const found = countArgumentTypes(args);
+                const error = compareExpectedArguments(
+                    expected, found.numbers, found.functions,
+                    1 + found.sequences, found.invalid
+                );
+                if(error) throw (
+                    `Error validating arguments for function "${name}": ${error}`
+                );
+            };
+            if(numbers === 1 || functions === 1){
+                return function(){
+                    validate(arguments);
+                    return implementation(arguments[0], this);
+                };
+            }else{
+                return function(){
+                    validate(arguments);
+                    return implementation(arguments, this);
+                };
+            }
+        }
+    }
+    return function(){
+        Array.prototype.splice.call(arguments, 0, 0, this)
+        return validateArguments(expected, arguments, implementation, function(error){
+            throw `Error validating arguments for function "${name}": ${error}`;
+        });
+    };
+};
+
+hi.internal.getWrapper.async = function(callback){
     return function(){
         return new Promise((resolve, reject) => {
             let args = arguments;
             hi.callAsync(() => resolve(callback(this, args)));
         });
     };
-};
-
-const hi = {
-    version: "0.1.0",
-    
-    isSequence: isSequence,
-    asSequence: asSequence,
-    seq: asSequence,
-    
-    registeredFunctions: [],
-    register: function(name, expected, implementation){
-        let register = {
-            name: name,
-            expected: expected,
-            implementation: implementation,
-            fancy: getFancyWrapperFunction(name, expected, implementation),
-        };
-        this[name] = register.fancy;
-        if(!expectNoArgument(expected.sequences)){
-            prototypeMethod = getPrototypeWrapperFunction(
-                name, expected, implementation
-            );
-            Sequence.prototype[name] = prototypeMethod;
-            register.prototypeMethod = prototypeMethod;
-        }
-        if(expected.async){
-            const fancyAsync = getAsyncWrapperFunction(
-                (caller, args) => fancy.apply(caller, args)
-            );
-            this[name + "Async"] = fancyAsync;
-            register.fancyAsync = fancyAsync;
-        }
-        if(expected.async && register.prototypeMethod){
-            const protoAsync = getAsyncWrapperFunction(
-                (caller, args) => prototypeMethod.apply(caller, args)
-            );
-            Sequence.prototype[name + "Async"] = protoAsync;
-            register.prototypeMethodAsync = protoAsync;
-        }
-        this.registeredFunctions.push(register);
-        return register.fancy;
-    },
-    alias: function(name, alias){
-        if(name in this){
-            this[alias] = this[name];
-        }
-        if(name + "Async" in this){
-            this[alias + "Async"] = this[name + "Async"];
-        }
-    },
 };
 
 if(typeof window === "undefined"){
