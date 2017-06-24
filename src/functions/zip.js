321e4f4e393fbc7de042f2b42bb4e0038e008569
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-24 23:09:39 +0300

    Update zip function to not be broken

--- src/functions/zip.js
+++ src/functions/zip.js
@@ -1,6 +1,11 @@
 // Simple abstraction of plural map function.
-// TODO: Fix this broken messs
 hi.zip = function(){
-    let transform = function(){return Array.prototype.slice(arguments);};
-    return map.raw(transform, arguments);
+    let sequences = [];
+    for(let argument of arguments){
+        sequences.push(hi.asSequence(argument));
+    }
+    let transform = function(){
+        return Array.prototype.slice.call(arguments);
+    };
+    return hi.map.raw(transform, sequences);
 }
