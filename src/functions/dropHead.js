e68f53e0b15b2cdb92175e97712291a5dda03363
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-24 23:01:08 +0300

    Added dropHead and dropTail functions

---
+++ src/functions/dropHead.js
@@ -0,0 +1,71 @@
+hi.DropHeadSequence = function(dropElements, source, initialized = false){
+    this.dropElements = dropElements;
+    this.source = source;
+    this.initialized = initialized;
+    this.maskAbsentMethods(source);
+}
+
+hi.DropHeadSequence.prototype = Object.create(hi.Sequence.prototype);
+Object.assign(hi.DropHeadSequence.prototype, {
+    initialize: function(){
+        this.initialized = true;
+        for(let i = 0; i < this.dropElements && !this.source.done(); i++){
+            this.source.popFront();
+        }
+    },
+    bounded: function(){
+        return this.source.bounded();
+    },
+    done: function(){
+        if(!this.initialized) this.initialize();
+        return this.source.done();
+    },
+    length: function(){
+        return this.source.length() - this.dropElements;
+    },
+    left: function(){
+        return this.source.left() - this.initialized * this.dropElements;
+    },
+    front: function(){
+        if(!this.initialized) this.initialize();
+        return this.source.front();
+    },
+    popFront: function(){
+        if(!this.initialized) this.initialize();
+        this.source.popFront();
+    },
+    back: null,
+    popBack: null,
+    index: function(i){
+        return this.source.index(i + this.dropElements);
+    },
+    // Don't create DropHeadSequences for sequences that already have slicing!
+    slice: null,
+    has: null,
+    get: function(i){
+        return this.source.get(i);
+    },
+    copy: function(){
+        let copy = new hi.DropHeadSequence(
+            this.dropElements, this.source.copy(), this.initialized
+        );
+    },
+    reset: function(){
+        this.source.reset();
+        this.DropHeadSequence = false;
+        return this;
+    },
+});
+
+hi.register("dropHead", {
+    numbers: 1,
+    sequences: 1,
+}, function(dropElements, source){
+    if(dropElements <= 0){
+        return source;
+    }else if(source.slice && source.length){
+        return source.slice(dropElements, source.length());
+    }else{
+        return new hi.DropHeadSequence(dropElements, source);
+    }
+});
---
+++ src/functions/dropTail.js
@@ -0,0 +1,73 @@
+hi.DropTailSequence = function(dropElements, source, frontIndex = 0){
+    if(!source.length){
+        throw "Error dropping tail: Input sequence must have known length.";
+    }
+    this.dropElements = dropElements;
+    this.source = source;
+    this.frontIndex = frontIndex;
+    this.lastIndex = source.length() - dropElements;
+    this.maskAbsentMethods(source);
+}
+
+hi.DropTailSequence.prototype = Object.create(hi.Sequence.prototype);
+Object.assign(hi.DropTailSequence.prototype, {
+    bounded: function(){
+        return this.source.bounded();
+    },
+    done: function(){
+        return this.frontIndex >= this.lastIndex;
+    },
+    length: function(){
+        return this.source.length() - this.dropElements;
+    },
+    left: function(){
+        return this.lastIndex - this.frontIndex;
+    },
+    front: function(){
+        return this.source.front();
+    },
+    popFront: function(){
+        this.source.popFront();
+        this.frontIndex++;
+    },
+    back: null,
+    popBack: null,
+    index: function(i){
+        return this.source.index(i);
+    },
+    // Don't create DropTailSequences for sequences that already have slicing!
+    slice: null,
+    has: null,
+    get: function(i){
+        return this.source.get(i);
+    },
+    copy: function(){
+        let copy = new hi.DropTailSequence(
+            this.dropElements, this.source.copy(), this.frontIndex
+        );
+    },
+    reset: function(){
+        this.source.reset();
+        this.frontIndex = 0;
+        return this;
+    },
+});
+
+hi.register("dropTail", {
+    numbers: 1,
+    sequences: 1,
+}, function(dropElements, source){
+    if(dropElements <= 0){
+        return source;
+    }else if(source.slice && source.length){
+        return source.slice(0, source.length() - dropElements);
+    }else if(source.length){
+        return new hi.DropTailSequence(dropElements, source);
+    }else if(source.bounded()){
+        // Sequence must be loaded into memory to perform the operation.
+        let array = hi.array.implementation(-1, source);
+        return array.slice(0, array.length() - dropElements);
+    }else{
+        throw "Failed to drop sequence tail: Input is unbounded.";
+    }
+});
