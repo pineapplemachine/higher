dacdf79410da5f1e0fab667f45cc077da0a831da
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-25 21:32:44 +0300

    Add product function

---
+++ src/functions/product.js
@@ -0,0 +1,13 @@
+// Get the product of the numbers in a sequence.
+// Returns 1 when the input is empty.
+hi.register("product", {
+    sequences: 1,
+    // Don't waste time coercing input iterables to sequences
+    allowIterables: true,
+    // Also generate an async version of this function
+    async: true,
+}, function(source){
+    let product = 1;
+    for(let value of source) product *= value;
+    return product;
+});
