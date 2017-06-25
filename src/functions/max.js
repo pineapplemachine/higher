e078b424f72d077111e0c4c0d68bf97927dbb909
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-25 21:41:56 +0300

    Just put min/max into separate modules
    meehhhhhhhh

---
+++ src/functions/max.js
@@ -0,0 +1,17 @@
+// Get the maximum value in a sequence as judged by a comparison function
+// If no comparison function is provided, then (a, b) => (a < b) is used.
+// Returns undefined when the input sequence was empty.
+hi.register("max", {
+    functions: 1,
+    sequences: 1,
+    // Don't waste time coercing input iterables to sequences
+    allowIterables: true,
+    // Also generate an async version of this function
+    async: true,
+}, function(compare, source){
+    let combine = (compare ?
+        (a, b) => (compare(b, a) ? a : b) :
+        (a, b) => (b < a ? a : b)
+    );
+    return hi.reduce.raw(combine, source).last();
+});
--- src/functions/min.js
+++ src/functions/min.js
@@ -15,19 +15,3 @@ hi.register("min", {
     );
     return hi.reduce.raw(combine, source).last();
 });
-
-// Get the maximum value in a sequence as judged by a comparison function
-// If no comparison function is provided, then (a, b) => (a < b) is used.
-// Returns undefined when the input sequence was empty.
-hi.register("max", {
-    functions: 1,
-    sequences: 1,
-    allowIterables: true,
-    async: true,
-}, function(compare, source){
-    let combine = (compare ?
-        (a, b) => (compare(b, a) ? a : b) :
-        (a, b) => (b < a ? a : b)
-    );
-    return hi.reduce.raw(combine, source).last();
-});
