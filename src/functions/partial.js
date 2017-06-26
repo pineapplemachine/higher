5edbb878d3ee304c715d414ea9c305871072b930
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-26 19:52:33 +0300

    Add partial functions

---
+++ src/functions/partial.js
@@ -0,0 +1,20 @@
+// Generate a partially-applied function.
+hi.partial = function(target, leftArgs, rightArgs){
+    return (...callArgs) => {
+        let useArgs = callArgs;
+        if(leftArgs) useArgs.unshift(...leftArgs);
+        if(rightArgs) useArgs.push(...rightArgs);
+        return target(...useArgs);
+    };
+};
+
+// Generate a partially-applied function.
+// The sequence the method is called for will be the left-most argument.
+hi.Sequence.prototype.partial = function(target, leftArgs, rightArgs){
+    return (...callArgs) => {
+        let useArgs = callArgs;
+        if(leftArgs) useArgs.unshift(...leftArgs);
+        if(rightArgs) useArgs.push(...rightArgs);
+        return target(this, ...useArgs);
+    };
+};
