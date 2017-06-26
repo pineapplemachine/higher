6bdde52e9a2f8671cb480d088aed79f627bf9bfb
Sophie Kirschner <sophie@thehumangeo.com>
2017-06-26 20:16:16 +0300

    Add pipe function

---
+++ src/functions/pipe.js
@@ -0,0 +1,16 @@
+// Returns a function which, when called the left-most function passed at
+// creation is evaluated, then that result is passed to the next function,
+// that result passed to the next, etc, until a final value is acquired.
+hi.pipe = function(rootFunction, ...moreFunctions){
+    if(!moreFunctions.length){
+        return rootFunction;
+    }else{
+        return (...args) => {
+            let value = rootFunction(...args);
+            for(let func of moreFunctions){
+                value = func(value);
+            }
+            return value;
+        };
+    }
+};
