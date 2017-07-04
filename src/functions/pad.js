80404c64751ad6481e3effb592d1ba1e4a1379a4
Sophie Kirschner <sophie@thehumangeo.com>
2017-07-04 10:42:21 +0300

    Add right-pad functionality to pad.js

--- src/functions/pad.js
+++ src/functions/pad.js
@@ -13,6 +13,8 @@ Object.assign(hi.SequencePadder.prototype, {
         const sourceLength = this.source.length();
         if(sourceLength >= length){
             return this.source;
+        }else if(sourceLength === 0){
+            return new hi.FiniteRepeatElementSequence(length, element);
         }else{
             return this.leftCount(length - sourceLength, element);
         }
@@ -23,21 +25,50 @@ Object.assign(hi.SequencePadder.prototype, {
         );
     },
     right: function(length, element){
-        // TODO
+        if(this.source.unbounded()){
+            return this.source;
+        }else if(!this.source.bounded()){
+            throw "Failed to pad sequence: Input must be known bounded or unbounded.";
+        }
+        if(!this.source.length) this.source.forceEager();
+        const sourceLength = this.source.length();
+        if(sourceLength >= length){
+            return this.source;
+        }else if(sourceLength === 0){
+            return new hi.FiniteRepeatElementSequence(length, element);
+        }else{
+            return this.rightCount(length - sourceLength, element);
+        }
     },
     rightCount: function(count, element){
-        // TODO
+        return count <= 0 ? source : new hi.PadRightSequence(
+            this.source, element, count
+        );
     },
 });
 
 hi.PadLeftSequence = function(
-    source, padElement, leftCount, leftPadded = undefined
+    source, padElement, padTotal, padCount = undefined
 ){
     this.source = source;
     this.padElement = padElement;
-    this.leftCount = leftCount;
-    this.leftPadded = leftPadded || 0;
+    this.padTotal = padTotal;
+    this.padCount = padCount || 0;
     this.maskAbsentMethods(source);
+};
+
+hi.PadRightSequence = function(
+    source, padElement, padTotal, padCount = undefined
+){
+    this.source = source;
+    this.padElement = padElement;
+    this.padTotal = padTotal;
+    this.padCount = padCount || 0;
+    this.maskAbsentMethods(source);
+    if(!source.length){
+        this.index = null;
+        this.slice = null;
+    }
 };
 
 hi.PadLeftSequence.prototype = Object.create(hi.Sequence.prototype);
@@ -47,22 +78,22 @@ Object.assign(hi.PadLeftSequence.prototype, {
         return this.source.bounded();
     },
     done: function(){
-        return this.leftPadded >= this.leftCount && this.source.done();
+        return this.padCount >= this.padTotal && this.source.done();
     },
     length: function(){
-        return this.source.length() + this.leftCount;
+        return this.source.length() + this.padTotal;
     },
     left: function(){
-        return this.source.left() + (this.leftCount - this.leftPadded);
+        return this.source.left() + (this.padTotal - this.padCount);
     },
     front: function(){
-        return (this.leftPadded >= this.leftCount ?
+        return (this.padCount >= this.padTotal ?
             this.source.front() : this.padElement
         );
     },
     popFront: function(){
-        this.leftPadded++;
-        if(this.leftPadded > this.leftCount){
+        this.padCount++;
+        if(this.padCount > this.padTotal){
             return this.source.popFront();
         }
     },
@@ -71,25 +102,25 @@ Object.assign(hi.PadLeftSequence.prototype, {
     },
     popBack: function(){
         if(this.source.done()){
-            this.leftPadded++;
+            this.padCount++;
         }else{
             return this.source.popBack();
         }
     },
     index: function(i){
-        return (i < this.leftCount ?
-            this.padElement : this.source.index(i - this.leftCount)
+        return (i < this.padTotal ?
+            this.padElement : this.source.index(i - this.padTotal)
         );
     },
     slice: function(i, j){
-        if(j < this.leftCount){
+        if(j < this.padTotal){
             return new hi.FiniteRepeatElementSequence(j - i, this.padElement);
-        }else if(i >= this.leftCount){
-            return this.source.slice(i - this.leftCount, j - this.leftCount);
+        }else if(i >= this.padTotal){
+            return this.source.slice(i - this.padTotal, j - this.padTotal);
         }else{
             return new hi.PadLeftSequence(
-                this.source.slice(0, j - this.leftCount),
-                this.padElement, this.leftCount - i
+                this.source.slice(0, j - this.padTotal),
+                this.padElement, this.padTotal - i
             );
         }
     },
@@ -102,12 +133,83 @@ Object.assign(hi.PadLeftSequence.prototype, {
     copy: function(){
         return new hi.PadLeftSequence(
             this.source.copy(), this.padElement,
-            this.leftCount, this.leftPadded
+            this.padTotal, this.padCount
         );
     },
     reset: function(){
         this.source.reset();
-        this.leftPadded = 0;
+        this.padCount = 0;
+        return this;
+    },
+});
+
+hi.PadRightSequence.prototype = Object.create(hi.Sequence.prototype);
+hi.PadRightSequence.prototype.constructor = hi.PadRightSequence;
+Object.assign(hi.PadRightSequence.prototype, {
+    bounded: function(){
+        return this.source.bounded();
+    },
+    done: function(){
+        return this.padCount >= this.padTotal && this.source.done();
+    },
+    length: function(){
+        return this.source.length() + this.padTotal;
+    },
+    left: function(){
+        return this.source.left() + (this.padTotal - this.padCount);
+    },
+    front: function(){
+        return this.source.done() ? this.padElement : this.source.front();
+    },
+    popFront: function(){
+        if(this.source.done()){
+            this.padCount++;
+        }else{
+            return this.source.popFront();
+        }
+    },
+    back: function(){
+        return (this.padCount >= this.padTotal ?
+            this.source.back() : this.padElement
+        );
+    },
+    popBack: function(){
+        this.padCount++;
+        if(this.padCount > this.padTotal){
+            return this.source.popBack();
+        }
+    },
+    index: function(i){
+        return i >= this.source.length() ? this.padElement : this.source.index(i);
+    },
+    slice: function(i, j){
+        const sourceLength = this.source.length();
+        if(i >= sourceLength){
+            return new hi.FiniteRepeatElementSequence(j - i, this.padElement);
+        }else if(j < sourceLength){
+            return this.source.slice(i, j);
+        }else{
+            return new hi.PadRightSequence(
+                this.source.slice(i, sourceLength),
+                this.padElement, j - sourceLength
+            );
+        }
+    },
+    has: function(i){
+        return this.source.has(i);
+    },
+    get: function(i){
+        return this.source.get(i);
+    },
+    copy: function(){
+        return new hi.PadLeftSequence(
+            this.source.copy(), this.padElement,
+            this.padTotal, this.padCount
+        );
+    },
+    reset: function(){
+        this.source.reset();
+        this.padCount = 0;
         return this;
     },
 });
