17d1e59f9060ca5b9cd8bb1a0ad574a97de71919
Sophie Kirschner <sophie@thehumangeo.com>
2017-07-12 16:06:35 +0300

    Update testRunner script

--- src/test/testRunner.js
+++ src/test/testRunner.js
@@ -5,19 +5,19 @@ console.log(`Running tests for higher@${hi.version}.`);
 const results = hi.test();
 
 const functionNames = [];
-for(const functionName in results) functionNames.push(functionName);
+for(const functionName in results.functions) functionNames.push(functionName);
 functionNames.sort();
 
 for(const name of functionNames){
-    const passed = results[name].pass.length;
-    const failed = results[name].fail.length;
+    const passed = results.functions[name].pass.length;
+    const failed = results.functions[name].fail.length;
     const total = passed + failed;
     console.log(`${name}: passed ${passed} of ${total}`);
 }
 
 let failures = 0;
 for(const name of functionNames){
-    for(const failure of results[name].fail){
+    for(const failure of results.functions[name].fail){
         console.error(`${name}.${failure.name} failed: ${failure.error.stack}`);
         failures++;
     }
