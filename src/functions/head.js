a925417b5d76f96a4d68d1d247dc0d9f2208bc54
Sophie Kirschner <sophie@thehumangeo.com>
2017-07-17 21:26:06 +0300

    Add docs and tests to head.js

--- src/functions/head.js
+++ src/functions/head.js
@@ -5,10 +5,37 @@ import {EmptySequence} from "./emptySequence";
 
 // Fallback implementation of head function for when slicing is unavailable.
 export const HeadSequence = Sequence.extend({
-    constructor: function HeadSequence(headLength, source, frontIndex = 0){
+    summary: "Enumerate only the first so many elements of a sequence.",
+    supportsWith: [
+        "length", "left", "get", "copy", "reset"
+    ],
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The constructor expects a number of elements to take from the
+            beginning of the input, and an input sequence.
+        `),
+    },
+    getSequence: process.env.NODE_ENV !== "development" ? undefined : [
+        hi => new HeadSequence(0, hi.emptySequence()),
+        hi => new HeadSequence(3, hi.emptySequence()),
+        hi => new HeadSequence(0, hi([0, 1, 2, 3, 4])),
+        hi => new HeadSequence(3, hi([0, 1, 2, 3, 4])),
+        hi => new HeadSequence(5, hi([0, 1, 2, 3, 4])),
+        hi => new HeadSequence(10, hi([0, 1, 2, 3, 4])),
+        hi => new HeadSequence(0, hi.counter()),
+        hi => new HeadSequence(3, hi.counter()),
+        hi => new HeadSequence(0, hi.counter().until(i => i >= 8)),
+        hi => new HeadSequence(3, hi.counter().until(i => i >= 8)),
+        hi => new HeadSequence(8, hi.counter().until(i => i >= 8)),
+        hi => new HeadSequence(10, hi.counter().until(i => i >= 8)),
+    ],
+    constructor: function HeadSequence(
+        headLength, source, frontIndex = undefined
+    ){
         this.headLength = headLength;
         this.source = source;
-        this.frontIndex = frontIndex;
+        this.frontIndex = frontIndex || 0;
         this.maskAbsentMethods(source);
     },
     bounded: () => true,
@@ -61,11 +88,28 @@ export const HeadSequence = Sequence.extend({
 // will never be longer.
 export const head = wrap({
     names: ["head", "take"],
+    summary: "Get a sequence enumerating the first so many elements of an input.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects as arguments one input sequence and a number
+            indicating the number of elements to take from its beginning.
+        `),
+        returns: (`
+            The function returns a sequence enumerating only up to the first
+            so many elements of the input. If there were fewer elements in the
+            input than the specified number, then the resulting sequence will
+            be only as long as the input.
+        `),
+        examples: [
+            "basicUsage",
+        ],
+        related: [
+            "head", "last", "dropTail",
+        ],
+    },
     attachSequence: true,
     async: false,
-    sequences: [
-        HeadSequence
-    ],
     arguments: {
         unordered: {
             numbers: 1,
@@ -76,11 +120,46 @@ export const head = wrap({
         if(elements < 1){
             return new EmptySequence();
         }else if(source.length && source.slice){
-            const length = source.length();
-            return source.slice(0, length < elements ? length : elements);
+            const sourceLength = source.length();
+            return sourceLength <= elements ? source : source.slice(0, elements);
         }else{
             return new HeadSequence(elements, source);
         }
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const array = [1, 2, 3, 4, 5, 6];
+            hi.assertEqual(hi.head(array, 4), [1, 2, 3, 4]);
+        },
+        "zeroLength": hi => {
+            hi.assertEmpty(hi.head(0, hi.emptySequence()));
+            hi.assertEmpty(hi.head(0, [0, 1, 2, 3]));
+            hi.assertEmpty(hi.head(0, hi.counter()));
+        },
+        "emptyInput": hi => {
+            hi.assertEmpty(hi.head(1, hi.emptySequence()));
+            hi.assertEmpty(hi.head(10, hi.emptySequence()));
+        },
+        "boundedInputs": hi => {
+            hi.assertEqual(hi.head(1, [0, 1, 2, 3, 4]), [0]);
+            hi.assertEqual(hi.head(3, [0, 1, 2, 3, 4]), [0, 1, 2]);
+            hi.assertEqual(hi.head(2, "hello"), "he");
+            hi.assertEqual(hi.head(4, "red"), "red");
+            hi.assertEqual(hi.head(100, "red"), "red");
+        },
+        "notKnownBoundedInput": hi => {
+            const seq = () => hi.recur(i => i + 1).seed(3).until(i => i >= 6);
+            hi.assertEmpty(seq().head(0));
+            hi.assertEqual(seq().head(1), [3]);
+            hi.assertEqual(seq().head(2), [3, 4]);
+            hi.assertEqual(seq().head(3), [3, 4, 5]);
+            hi.assertEqual(seq().head(4), [3, 4, 5]);
+            hi.assertEqual(seq().head(100), [3, 4, 5]);
+        },
+        "unboundedInput": hi => {
+            hi.assertEqual(hi.counter().head(1), [0]);
+            hi.assertEqual(hi.counter().head(4), [0, 1, 2, 3]);
+        },
     },
 });
 
