f47d7457aa65ef962f0b8286a8d7124c8da9514a
Sophie Kirschner <sophie@thehumangeo.com>
2017-07-19 16:10:41 +0300

    Add documentation and tests to partition function

--- src/functions/partition.js
+++ src/functions/partition.js
@@ -1,8 +1,30 @@
-import {validAsBoundedSequence} from "../core/asSequence";
 import {wrap} from "../core/wrap";
+
+import {NotBoundedError} from "../errors/NotBoundedError";
 
 export const partition = wrap({
     name: "partition",
+    summary: "Partition the elements of a sequence by a predicate into two arrays.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects as input a predicate function and a known-
+            bounded input sequence.
+        `),
+        returns: (`
+            The function returns an array containing two more arrays; the first
+            contained array contains those elements of the input that did not
+            satisfy the predicate function and the second array contains those
+            elements of the input that did satisfy the predicate.
+        `),
+        throws: (`
+            The function throws a @NotBoundedError when the input sequence was
+            not known to be bounded.
+        `),
+        examples: [
+            "basicUsage",
+        ],
+    },
     attachSequence: true,
     async: true,
     arguments: {
@@ -13,9 +35,9 @@ export const partition = wrap({
         }
     },
     implementation: (predicate, source) => {
-        if(!validAsBoundedSequence(source)){
-            throw "Failed to partition sequence: Can't partition an unbounded sequence.";
-        }
+        NotBoundedError.enforce(source, {
+            message: "Failed to partition sequence",
+        });
         const a = [];
         const b = [];
         for(const element of source){
@@ -23,6 +45,29 @@ export const partition = wrap({
             else b.push(element);
         }
         return [a, b];
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const strings = ["hello", "there", "how", "are", "you"];
+            const partitions = hi.partition(strings, i => i[0] === "h");
+            hi.assertEqual(partitions[0], ["hello", "how"]);
+            hi.assertEqual(partitions[1], ["there", "are", "you"]);
+        },
+        "emptyInput": hi => {
+            const partitions = hi.partition(hi.emptySequence(), i => true);
+            hi.assertEmpty(partitions[0]);
+            hi.assertEmpty(partitions[1]);
+        },
+        "allSatisfyPredicate": hi => {
+            const partitions = hi.partition([2, 4, 6], i => i % 2 === 0);
+            hi.assertEqual(partitions[0], [2, 4, 6]);
+            hi.assertEmpty(partitions[1]);
+        },
+        "noneSatisfyPredicate": hi => {
+            const partitions = hi.partition([2, 4, 6], i => i % 2 !== 0);
+            hi.assertEmpty(partitions[0]);
+            hi.assertEqual(partitions[1], [2, 4, 6]);
+        },
     },
 });
 
