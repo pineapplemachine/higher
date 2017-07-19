b1ea4cabd0a9b54f87554092c192d79f3ec310d6
Sophie Kirschner <sophie@thehumangeo.com>
2017-07-19 21:01:08 +0300

    Add docs and tests to until.js

--- src/functions/until.js
+++ src/functions/until.js
@@ -2,22 +2,71 @@ import {Sequence} from "../core/sequence";
 import {wrap} from "../core/wrap";
 
 export const UntilSequence = Sequence.extend({
+    summary: "Enumerate elements in the input sequence until one satisfying a predicate is found.",
+    supportsWith: [
+        "index", "copy", "reset",
+    ],
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The constructor expects as its arguments a predicate function and
+            an input sequence.
+        `),
+        methods: {
+            "inclusive": {
+                introduced: "higher@1.0.0",
+                summary: "Cause the sequence to include the first element satisfying the predicate.",
+                returns: (`
+                    The function returns the sequence itself.
+                `),
+                examples: [
+                    "basicUsage",
+                ],
+                tests: {
+                    "basicUsage": hi => {
+                        const beeping = () => hi("beeping");
+                        hi.assertEqual(beeping().until(i => i === "p"), "bee");
+                        hi.assertEqual(beeping().until(i => i === "p").exclusive(), "bee");
+                        hi.assertEqual(beeping().until(i => i === "p").inclusive(), "beep");
+                    },
+                },
+            },
+            "exclusive": {
+                introduced: "higher@1.0.0",
+                summary: "Cause the sequence to exclude the first element satisfying the predicate.",
+                returns: (`
+                    The function returns the sequence itself.
+                `),
+                examples: [
+                    "basicUsage",
+                ],
+                tests: {
+                    "basicUsage": hi => {
+                        const beeping = () => hi("beeping");
+                        hi.assertEqual(beeping().until(i => i === "p").exclusive(), "bee");
+                        hi.assertEqual(beeping().until(i => i === "p").inclusive(), "beep");
+                    },
+                },
+            },
+        },
+    },
+    getSequence: process.env.NODE_ENV !== "development" ? undefined : [
+        hi => new UntilSequence(i => true, [1, 2, 3]),
+        hi => new UntilSequence(i => true, [1, 2, 3]).inclusive(),
+        hi => new UntilSequence(i => false, [1, 2, 3]),
+        hi => new UntilSequence(i => false, [1, 2, 3]).inclusive(),
+        hi => new UntilSequence(i => i >= 4, [1, 2, 3, 4, 5, 6]),
+        hi => new UntilSequence(i => i >= 4, [1, 2, 3, 4, 5, 6]).inclusive(),
+        hi => new UntilSequence(i => i >= 6, hi.counter()),
+        hi => new UntilSequence(i => i >= 6, hi.counter()).inclusive(),
+    ],
     constructor: function UntilSequence(
-        predicate, source, isInclusive = false, included = true,
-        satisfied = false, frontValue = null, initialize = true
+        predicate, source, isInclusive = undefined, included = undefined
     ){
         this.predicate = predicate;
         this.source = source;
-        this.isInclusive = isInclusive;
-        this.included = included;
-        if(source.done() || !initialize){
-            this.frontValue = frontValue;
-            this.satisfied = satisfied;
-        }else{
-            // TODO: This should be in an initialization method
-            this.frontValue = source.nextFront();
-            this.satisfied = predicate(this.frontValue);
-        }
+        this.isInclusive = isInclusive || false;
+        this.included = included || false;
         this.maskAbsentMethods(source);
     },
     inclusive: function(){
@@ -27,7 +76,6 @@ export const UntilSequence = Sequence.extend({
     },
     exclusive: function(){
         this.isInclusive = false;
-        this.included = true;
         return this;
     },
     bounded: function(){
@@ -35,19 +83,20 @@ export const UntilSequence = Sequence.extend({
     },
     unbounded: () => false,
     done: function(){
-        return (this.satisfied || this.source.done()) && this.included;
+        return this.source.done() || (
+            this.predicate(this.source.front()) && (this.included || !this.isInclusive)
+        );
     },
     length: null,
     left: null,
     front: function(){
-        return this.frontValue;
+        return this.source.front();
     },
     popFront: function(){
-        if(this.satisfied){
+        if(this.predicate(this.source.front())){
             this.included = true;
         }else{
-            this.frontValue = this.source.nextFront();
-            this.satisfied = this.predicate(this.frontValue);
+            this.source.popFront();
         }
     },
     back: null,
@@ -57,37 +106,52 @@ export const UntilSequence = Sequence.extend({
     },
     slice: null,
     has: null,
-    get: function(i){
-        return this.source.get(i);
-    },
+    get: null,
     copy: function(){
         return new UntilSequence(
-            this.predicate, this.source.copy(), this.isInclusive,
-            this.included, this.satisfied, this.frontValue, false
+            this.predicate, this.source.copy(),
+            this.isInclusive, this.included
         );
     },
     reset: function(){
         this.source.reset();
-        this.included = !this.isInclusive;
-        if(!this.source.done()){
-            this.frontValue = this.source.nextFront();
-            this.satisfied = this.predicate(this.frontValue);
-        }
+        this.included = false;
         return this;
     },
     rebase: function(source){
         this.source = source;
-        if(!this.source.done()){
-            // TODO: This should be in an initialization method
-            this.frontValue = this.source.nextFront();
-            this.satisfied = this.predicate(this.frontValue);
-        }
         return this;
     },
 });
 
 export const until = wrap({
     name: "until",
+    summary: "Enumerate elements in a sequence until one satisfying a predicate is found.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects a predicate function and a sequence as input.
+        `),
+        returns: (`
+            The function returns a sequence which enumerates the elements of
+            the input sequence up to the first element satisfying the predicate.
+            When no elements in the input sequence satisfied the predicate, the
+            output sequence enumerates the entirety of the input sequence.
+        `),
+        developers: (`
+            The default behavior of the outputted sequence is to enumerate
+            elements up to and not including the first element to satisfy the
+            predicate.
+            The outputted sequence may be made to include the terminating
+            element using its @UntilSequence.inclusive method.
+        `),
+        examples: [
+            "basicUsageExclusive", "basicUsageInclusive",
+        ],
+        related: [
+            "from",
+        ],
+    },
     attachSequence: true,
     async: false,
     sequences: [
@@ -101,6 +165,40 @@ export const until = wrap({
     },
     implementation: (predicate, source) => {
         return new UntilSequence(predicate, source);
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsageExclusive": hi => {
+            const seq = hi([1, 2, 3, 4, 5, 6]).until(i => i === 4);
+            hi.assertEqual(seq, [1, 2, 3]);
+        },
+        "basicUsageInclusive": hi => {
+            const seq = hi([1, 2, 3, 4, 5, 6]).until(i => i === 4).inclusive();
+            hi.assertEqual(seq, [1, 2, 3, 4]);
+        },
+        "firstElementMatched": hi => {
+            hi.assertEmpty(hi("hello").until(() => true));
+            hi.assertEmpty(hi("hello").until(() => true).exclusive());
+            hi.assertEqual(hi("hello").until(() => true).inclusive(), "h");
+        },
+        "noElementsMatched": hi => {
+            hi.assertEqual(hi("hello").until(() => false), "hello");
+            hi.assertEqual(hi("hello").until(() => false).exclusive(), "hello");
+            hi.assertEqual(hi("hello").until(() => false).inclusive(), "hello");
+        },
+        "emptyInput": hi => {
+            hi.assertEmpty(hi.emptySequence().until(i => i));
+        },
+        "notKnownBoundedInput": hi => {
+            const seq = () => hi.recur(i => i + 1).seed(3).until(i => i >= 6);
+            hi.assertEqual(seq(), [3, 4, 5]);
+            hi.assertEqual(seq().inclusive(), [3, 4, 5, 6]);
+        },
+        "unboundedInput": hi => {
+            const seq = hi.recur(i => 2 * -i).seed(1).until(i => i >= 16);
+            hi.assert(!seq.bounded());
+            hi.assert(!seq.unbounded());
+            hi.assertEqual(seq, [+1, -2, +4, -8]);
+        },
     },
 });
 
