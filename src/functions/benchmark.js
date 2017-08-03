d5804f2976ceccba439647095597796fd9f97728
Sophie Kirschner <sophie@thehumangeo.com>
2017-08-03 16:33:45 +0300

    Document and test time.js and benchmark.js

--- src/functions/benchmark.js
+++ src/functions/benchmark.js
@@ -6,15 +6,39 @@ import {time} from "./time";
 // times.
 export const benchmark = wrap({
     name: "benchmark",
+    summary: "Get the time in milliseconds it takes to repeatedly evaluate a function.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects a number and a function as input.
+        `),
+        returns: (`
+            The function returns the amount of time in milliseconds that it
+            took to evaluate the input function the given number of times.
+        `),
+        examples: [
+            "basicUsage",
+        ],
+        related: [
+            "time",
+        ],
+    },
     attachSequence: false,
     async: true,
     arguments: {
-        ordered: [wrap.expecting.number, wrap.expecting.function] // + ...args
+        ordered: [wrap.expecting.number, wrap.expecting.function]
     },
-    implementation: (count, call, ...args) => {
+    implementation: (repetitions, call) => {
         return time(() => {
-            for(let i = 0; i < count; i++) call(...args);
+            for(let i = 0; i < repetitions; i++) call();
         });
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const call = () => 5 + 5;
+            const millisecs = hi.benchmark(100, call);
+            hi.assert(millisecs < 100);
+        },
     },
 });
 
