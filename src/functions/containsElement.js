da99255759add90c3293992be5192739ed37fd71
Sophie Kirschner <sophie@thehumangeo.com>
2017-08-03 16:12:55 +0300

    Add tests and documentation to containsElement

--- src/functions/containsElement.js
+++ src/functions/containsElement.js
@@ -1,20 +1,55 @@
+import {isEqual} from "../core/isEqual";
 import {wrap} from "../core/wrap";
 
-// Check if a sequence contains an element. Uses the comparison function
-// (a, b) => (a === b). When a different comparison function is needed,
-// use hi.any instead. To search for a substring, use hi.find instead.
 export const containsElement = wrap({
     name: "containsElement",
+    summary: "Get whether a sequence contains any element equal to a given value.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects as inputs a known-bounded sequence and a
+            value to check each element for equality with.
+        `),
+        returns: (`
+            The function returns @true if any element in the input sequence was
+            equivalent to the given value according to the @isEqual function.
+            The function returns false otherwise, or if the input sequence was
+            empty.
+        `),
+        examples: [
+            "basicUsage",
+        ],
+        related: [
+            "find", "any",
+        ],
+    },
     attachSequence: true,
     async: true,
     arguments: {
-        ordered: [wrap.expecting.iterable, wrap.expecting.anything]
+        ordered: [wrap.expecting.boundedSequence, wrap.expecting.anything]
     },
     implementation: (source, element) => {
         for(const sourceElement of source){
-            if(element === sourceElement) return true;
+            if(isEqual(element, sourceElement)) return true;
         }
         return false;
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const strings = ["hello", "there", "friend"];
+            hi.assert(hi.containsElement(strings, "friend"));
+            hi.assertNot(hi.containsElement(strings, "enemy"));
+        },
+        "stringInput": hi => {
+            hi.assert(hi("abc").containsElement("a"));
+            hi.assertNot(hi("abc").containsElement("!"));
+        },
+        "emptyInput": hi => {
+            hi.assertNot(hi.emptySequence().containsElement(null));
+        },
+        "unboundedInput": hi => {
+            hi.assertFail(() => hi.repeat("!?").containsElement("x"));
+        },
     },
 });
 
