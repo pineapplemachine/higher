7557c50fc70179ca8a69c60df223d9a3ca4146a8
Sophie Kirschner <sophiek@pineapplemachine.com>
2017-08-04 11:44:50 +0300

    Document and test time.js and benchmark.js (#119)
    * Document and test time.js and benchmark.js
    
    * Hopefully make the test time intervals large enough to not break travis
    
    * Fix typo in time function implementation
    
    oops

--- src/functions/time.js
+++ src/functions/time.js
@@ -3,23 +3,46 @@ import {wrap} from "../core/wrap";
 export const time = wrap({
     name: "time",
     summary: "Get the time in milliseconds that it takes to evaluate a function.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects a single function as input.
+        `),
+        returns: (`
+            The function returns the amount of time in milliseconds that it
+            took to evaluate the input function once.
+        `),
+        examples: [
+            "basicUsage",
+        ],
+        related: [
+            "benchmark",
+        ],
+    },
     attachSequence: false,
     async: true,
     arguments: {
-        ordered: [wrap.expecting.function] // + ...args
+        ordered: [wrap.expecting.function]
     },
-    implementation: (typeof performance === "undefined" ?
-        (call, ...args) => {
+    implementation: (typeof performance !== "undefined" ?
+        call => {
             const start = performance.now();
-            call(...args);
+            call();
             return performance.now() - start;
         } :
-        (call, ...args) => {
+        call => {
             const start = new Date().getTime();
-            call(...args);
+            call();
             return new Date().getTime() - start;
         }
     ),
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const call = () => 10 + 10;
+            const millisecs = hi.time(call);
+            hi.assert(millisecs < 100);
+        },
+    },
 });
 
 export default time;
