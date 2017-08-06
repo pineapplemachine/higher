7b61134fc09d3e5aea834b3351b9a0f372d26684
Sophie Kirschner <sophie@thehumangeo.com>
2017-08-06 10:28:44 +0300

    Improve, document, and test count function
    Add countEquals, countLessThan, countGreaterThan, countAtLeast, countAtMost functions to serve the purposes the weirdly written count function did before

--- src/functions/count.js
+++ src/functions/count.js
@@ -1,115 +1,68 @@
-import {callAsync} from "../core/callAsync";
-import {constants} from "../core/constants";
 import {wrap} from "../core/wrap";
-
-export const SequenceCounter = function(predicate, source){
-    this.predicate = predicate;
-    this.source = source;
-    if(!source.copy) this.copy = null;
-    if(!source.reset) this.reset = null;
-};
-
-SequenceCounter.prototype.constructor = SequenceCounter;
-Object.assign(SequenceCounter.prototype, {
-    sum: function(){
-        let i = 0;
-        for(const element of this.source){
-            i += this.predicate(element) ? 1 : 0;
-        }
-        return i;
-    },
-    equals: function(n){
-        let i = 0;
-        for(const element of this.source){
-            i += this.predicate(element) ? 1 : 0;
-            if(i > n) return false;
-        }
-        return i === +n;
-    },
-    lessThan: function(n){
-        let i = 0;
-        for(const element of this.source){
-            i += this.predicate(element) ? 1 : 0;
-            if(i >= n) return false;
-        }
-        return true;
-    },
-    lessThanEqual: function(n){
-        let i = 0;
-        for(const element of this.source){
-            i += this.predicate(element) ? 1 : 0;
-            if(i > n) return false;
-        }
-        return true;
-    },
-    greaterThan: function(n){
-        let i = 0;
-        for(const element of this.source){
-            i += this.predicate(element) ? 1 : 0;
-            if(i > n) return true;
-        }
-        return false;
-    },
-    greaterThanEqual: function(n){
-        let i = 0;
-        for(const element of this.source){
-            i += this.predicate(element) ? 1 : 0;
-            if(i >= n) return true;
-        }
-        return false;
-    },
-    copy: function(){
-        return new SequenceCounter(this.predicate, this.source.copy());
-    },
-    reset: function(){
-        this.source.reset();
-        return this;
-    },
-    sumAsync: function(){
-        return new constants.Promise((resolve, reject) => {
-            callAsync(() => resolve(this.sum()));
-        });
-    },
-    equalsAsync: function(n){
-        return new constants.Promise((resolve, reject) => {
-            callAsync(() => resolve(this.equals(n)));
-        });
-    },
-    lessThanAsync: function(n){
-        return new constants.Promise((resolve, reject) => {
-            callAsync(() => resolve(this.lessThan(n)));
-        });
-    },
-    lessThanEqualAsync: function(n){
-        return new constants.Promise((resolve, reject) => {
-            callAsync(() => resolve(this.lessThanEqual(n)));
-        });
-    },
-    greaterThanAsync: function(n){
-        return new constants.Promise((resolve, reject) => {
-            callAsync(() => resolve(this.greaterThan(n)));
-        });
-    },
-    greaterThanEqualAsync: function(n){
-        return new constants.Promise((resolve, reject) => {
-            callAsync(() => resolve(this.greaterThanEqual(n)));
-        });
-    },
-});
 
 export const count = wrap({
     name: "count",
+    summary: "Get the number of elements in a sequence that satisfy a predicate.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects a known-bounded sequence and a predicate
+            function as input.
+        `),
+        returns: (`
+            The function returns the number of elements in the input sequence
+            which satisfied the predicate.
+            The function returns #0 when the input sequence was empty.
+        `),
+        developers: (`
+            When the purpose of finding the count is to compare it to some
+            number, consider using @countEquals, @countLessThan, @countGreaterThan,
+            @countAtLeast, and @countAtMost instead. Where this function must
+            always fully consume the input sequence, those companion functions
+            are not always so compelled.
+        `),
+        returnType: "number",
+        examples: [
+            "basicUsage",
+        ],
+        related: [
+            "countBy", "countEquals", "countLessThan",
+            "countGreaterThan", "countAtLeast", "countAtMost",
+        ],
+    },
     attachSequence: true,
-    async: false,
+    async: true,
     arguments: {
         unordered: {
-            functions: 1,
-            sequences: 1,
-            allowIterables: true
+            functions: {one: wrap.expecting.predicate},
+            sequences: {one: wrap.expecting.boundedSequence},
         }
     },
     implementation: (predicate, source) => {
-        return new SequenceCounter(predicate, source);
+        let i = 0;
+        for(const element of source) i += !!predicate(element);
+        return i;
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const even = i => i % 2 === 0;
+            const lessThanFive = i => i < 5;
+            const array = [1, 2, 3, 4, 5, 6];
+            hi.assert(hi.count(array, even) === 3);
+            hi.assert(hi.count(array, lessThanFive) === 4);
+        },
+        "noElementsSatisfy": hi => {
+            hi.assert(hi.range(12).count(i => false) === 0);
+        },
+        "allElementsSatisfy": hi => {
+            hi.assert(hi.range(12).count(i => true) === 12);
+        },
+        "emptyInput": hi => {
+            hi.assert(hi.emptySequence().count(i => true) === 0);
+        },
+        "unboundedInput": hi => {
+            hi.assertFail(() => hi.counter().count(i => true));
+        },
     },
 });
 
---
+++ src/functions/countEquals.js
@@ -0,0 +1,78 @@
+import {wrap} from "../core/wrap";
+
+export const countEquals = wrap({
+    name: "countEquals",
+    summary: "Get whether the number of elements in a sequence satisfying a predicate is equal to some value.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The function expects a known-bounded sequence, a predicate
+            function, and an integer as input.
+        `),
+        returns: (`
+            The function returns #true when the number of elements in the input
+            sequence satisfying the predicate was exactly equal to the number
+            given.
+        `),
+        returnType: "number",
+        examples: [
+            "basicUsage",
+        ],
+        related: [
+            "count", "countLessThan", "countGreaterThan",
+            "countAtLeast", "countAtMost",
+        ],
+    },
+    attachSequence: true,
+    async: true,
+    arguments: {
+        unordered: {
+            numbers: 1,
+            functions: {one: wrap.expecting.predicate},
+            sequences: {one: wrap.expecting.boundedSequence},
+        }
+    },
+    implementation: (count, predicate, source) => {
+        // Count is never negative, infinite, or NaN or undefined.
+        if(count < 0 || !isFinite(count) || (!count && count !== 0)) return false;
+        // Count must always be less than the input sequence's length.
+        if(source.length && count > source.length()) return false;
+        // None of those conditions were met, so just consume the sequence.
+        let i = 0;
+        for(const element of source){
+            i += !!predicate(element);
+            if(i > count) return false;
+        }
+        return i === count;
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const array = [1, 2, 3, 4, 5, 6];
+            const even = i => i % 2 === 0;
+            const lessThanFive = i => i < 5;
+            hi.assert(hi.countEquals(3, array, even));
+            hi.assert(hi.countEquals(4, array, lessThanFive));
+        },
+        "noElementsSatisfy": hi => {
+            hi.assert(hi.range(12).countEquals(0, i => false));
+            hi.assertNot(hi.range(12).countEquals(2, i => false));
+        },
+        "allElementsSatisfy": hi => {
+            hi.assert(hi.range(12).countEquals(12, i => true));
+            hi.assertNot(hi.range(12).countEquals(0, i => true));
+            hi.assertNot(hi.range(12).countEquals(11, i => true));
+            hi.assertNot(hi.range(12).countEquals(13, i => true));
+            hi.assertNot(hi.range(12).countEquals(Infinity, i => true));
+            hi.assertNot(hi.range(12).countEquals(NaN, i => true));
+            hi.assertNot(hi.range(12).countEquals(-1, i => true));
+        },
+        "emptyInput": hi => {
+            hi.assert(hi.emptySequence().countEquals(0, i => true));
+        },
+        "unboundedInput": hi => {
+            hi.assertFail(() => hi.counter().countEquals(1, i => true));
+        },
+    },
+});
+
+export default countEquals;
