1a7b83f5ac603ff44feae7cbe049c1a7673b64d9
Sophie Kirschner <sophie@thehumangeo.com>
2017-08-20 11:34:02 +0300

    Update dropLast for new sequence method things

--- src/functions/dropLast.js
+++ src/functions/dropLast.js
@@ -7,28 +7,26 @@ import {HeadSequence} from "./head";
 
 export const DropLastSequence = defineSequence({
     summary: "Enumerate the contents of a sequence, omitting the last so many elements.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The constructor expects a number of elements to drop and a sequence
+            to drop them from as input.
+        `),
+    },
     constructor: function DropLastSequence(
         dropTarget, source, seenElements = undefined
     ){
         this.dropTarget = dropTarget;
         this.source = source;
-        this.seenElements = seenElements || [];
+        this.seenElements = seenElements;
         this.maskAbsentMethods(source);
     },
-    initialize: function(){
+    initializeFront: function(){
+        this.seenElements = [];
         while(this.seenElements.length < this.dropTarget && !this.source.done()){
             this.seenElements.push(this.source.nextFront());
         }
-        this.done = function(){
-            return this.source.done();
-        };
-        this.front = function(){
-            return this.seenElements[0];
-        };
-        this.popFront = function(){
-            this.seenElements.shift();
-            this.seenElements.push(this.source.nextFront());
-        };
     },
     bounded: function(){
         return this.source.bounded();
@@ -38,22 +36,20 @@ export const DropLastSequence = defineSequence({
         return this.source.unbounded();
     },
     done: function(){
-        this.initialize();
+        if(!this.seenElements) this.initializeFront();
         return this.source.done();
     },
     length: function(){
         return Math.max(0, this.source.length() - this.dropTarget);
     },
-    left: function(){
-        return Math.max(0, this.source.left() - this.dropTarget);
-    },
     front: function(){
-        this.initialize();
+        if(!this.seenElements) this.initializeFront();
         return this.seenElements[0];
     },
     popFront: function(){
-        this.initialize();
-        return this.popFront();
+        if(!this.seenElements) this.initializeFront();
+        this.seenElements.shift();
+        this.seenElements.push(this.source.nextFront());
     },
     index: function(i){
         return this.source.index(i);
@@ -62,25 +58,28 @@ export const DropLastSequence = defineSequence({
         return this.source.slice(i, j);
     },
     copy: function(){
-        const copy = new DropLastSequence(
-            this.dropTarget, this.source.copy(), this.seenElements.slice()
+        return new DropLastSequence(
+            this.dropTarget, this.source.copy(), (this.seenElements ?
+                this.seenElements.slice() : undefined
+            )
         );
-        copy.done = this.done;
-        copy.front = this.front;
-        copy.popFront = this.popFront;
-        return copy;
     },
-    reset: function(){
-        this.source.reset();
-        delete this.done;
-        delete this.front;
-        delete this.popFront;
+    rebase: function(source){
+        this.source = source;
         return this;
-    },
+    }
 });
 
 export const DropLastPredicateSequence = defineSequence({
     summary: "Enumerate the contents of a sequence, omitting the last so many elements satisfying a predicate.",
+    docs: process.env.NODE_ENV !== "development" ? undefined : {
+        introduced: "higher@1.0.0",
+        expects: (`
+            The constructor expects a number of elements to drop, a predicate
+            describing which elements to drop, and a sequence to drop them from
+            as input.
+        `),
+    },
     constructor: function DropLastPredicateSequence(
         dropTarget, predicate, source, frontIndex = undefined,
         seenElements = undefined, seenSatisfied = undefined
@@ -89,11 +88,13 @@ export const DropLastPredicateSequence = defineSequence({
         this.predicate = predicate;
         this.source = source;
         this.frontIndex = frontIndex || 0;
-        this.seenElements = seenElements || [];
-        this.seenSatisfied = seenSatisfied || [];
+        this.seenElements = seenElements;
+        this.seenSatisfied = seenSatisfied;
         this.maskAbsentMethods(source);
     },
-    initialize: function(){
+    initializeFront: function(){
+        this.seenElements = [];
+        this.seenSatisfied = [];
         while(this.seenSatisfied.length <= this.dropTarget && !this.source.done()){
             const element = this.source.nextFront();
             if(this.predicate(element)){
@@ -108,46 +109,6 @@ export const DropLastPredicateSequence = defineSequence({
                 this.seenElements.shift();
             }
         }
-        this.done = function(){
-            return this.seenElements.length === 0 && this.source.done();
-        };
-        this.front = function(){
-            return this.seenElements[0];
-        };
-        this.popFront = function(){
-            // Remove the element just viewed
-            this.seenElements.shift();
-            // While the source has yet to be fully consumed...
-            if(!this.source.done()){
-                const element = this.source.nextFront();
-                if(this.predicate(element)){
-                    this.seenSatisfied.shift();
-                    this.seenSatisfied.push(this.frontIndex);
-                }
-                this.seenElements.push(element);
-                this.frontIndex++;
-                if(this.seenSatisfied[0] <= this.frontIndex - this.seenElements.length - 1){
-                    this.seenSatisfied.shift();
-                    while(this.seenSatisfied.length <= this.dropTarget && !this.source.done()){
-                        const nextElement = this.source.nextFront();
-                        if(this.predicate(nextElement)){
-                            this.seenSatisfied.push(this.frontIndex);
-                        }
-                        this.seenElements.push(nextElement);
-                        this.frontIndex++;
-                    }
-                }else if(this.source.done()){
-                    this.seenSatisfied.shift();
-                }
-            }
-            // After the source was consumed and the last elements are being enumerated...
-            if(this.source.done()){
-                while(this.seenSatisfied[0] === this.frontIndex - this.seenElements.length){
-                    this.seenSatisfied.shift();
-                    this.seenElements.shift();
-                }
-            }
-        };
     },
     bounded: function(){
         return this.source.bounded();
@@ -157,33 +118,56 @@ export const DropLastPredicateSequence = defineSequence({
         return this.source.unbounded();
     },
     done: function(){
-        this.initialize();
+        if(!this.seenElements) this.initializeFront();
         return this.seenElements.length === 0 && this.source.done();
     },
     front: function(){
-        this.initialize();
+        if(!this.seenElements) this.initializeFront();
         return this.seenElements[0];
     },
     popFront: function(){
-        this.initialize();
-        return this.popFront();
+        if(!this.seenElements){
+            this.initializeFront();
+        }
+        // Remove the element just viewed
+        this.seenElements.shift();
+        // While the source has yet to be fully consumed...
+        if(!this.source.done()){
+            const element = this.source.nextFront();
+            if(this.predicate(element)){
+                this.seenSatisfied.shift();
+                this.seenSatisfied.push(this.frontIndex);
+            }
+            this.seenElements.push(element);
+            this.frontIndex++;
+            if(this.seenSatisfied[0] <= this.frontIndex - this.seenElements.length - 1){
+                this.seenSatisfied.shift();
+                while(this.seenSatisfied.length <= this.dropTarget && !this.source.done()){
+                    const nextElement = this.source.nextFront();
+                    if(this.predicate(nextElement)){
+                        this.seenSatisfied.push(this.frontIndex);
+                    }
+                    this.seenElements.push(nextElement);
+                    this.frontIndex++;
+                }
+            }else if(this.source.done()){
+                this.seenSatisfied.shift();
+            }
+        }
+        // After the source was consumed and the last elements are being enumerated...
+        if(this.source.done()){
+            while(this.seenSatisfied[0] === this.frontIndex - this.seenElements.length){
+                this.seenSatisfied.shift();
+                this.seenElements.shift();
+            }
+        }
     },
     copy: function(){
-        const copy = new DropLastPredicateSequence(
-            this.dropTarget, this.predicate, this.source.copy(),
-            this.frontIndex, this.seenElements.slice(), this.seenSatisfied.slice()
+        return new DropLastPredicateSequence(
+            this.dropTarget, this.predicate, this.source.copy(), this.frontIndex,
+            this.seenElements ? this.seenElements.slice() : undefined,
+            this.seenSatisfied ? this.seenSatisfied.slice() : undefined
         );
-        copy.done = this.done;
-        copy.front = this.front;
-        copy.popFront = this.popFront;
-        return copy;
-    },
-    reset: function(){
-        this.source.reset();
-        delete this.done;
-        delete this.front;
-        delete this.popFront;
-        return this;
     },
 });
 
@@ -193,7 +177,7 @@ export const UnboundedDropLastSequence = defineSequence({
         "back",
     ],
     supportsWith: [
-        "index", "slice", "copy", "reset",
+        "index", "slice", "copy",
     ],
     docs: process.env.NODE_ENV !== "development" ? undefined : {
         introduced: "higher@1.0.0",
@@ -209,19 +193,19 @@ export const UnboundedDropLastSequence = defineSequence({
         hi => new UnboundedDropLastSequence(0, hi.repeat("hello")),
         hi => new UnboundedDropLastSequence(10, hi.repeat("hello")),
     ],
-    constructor: function UnboundedDropLastSequence(dropTarget, source){
+    constructor: function UnboundedDropLastSequence(
+        dropTarget, source, initializedBack = undefined
+    ){
         this.dropTarget = dropTarget;
         this.source = source;
+        this.initializedBack = initializedBack;
         this.maskAbsentMethods(source);
     },
     initializeBack: function(){
-        for(let i = 0; i < this.dropTarget; i++) this.source.popBack();
-        this.back = function(){
-            return this.source.back();
-        };
-        this.popBack = function(){
-            return this.source.popBack();
-        };
+        this.initializedBack = true;
+        for(let i = 0; i < this.dropTarget; i++){
+            this.source.popBack();
+        }
     },
     bounded: () => false,
     unbounded: () => true,
@@ -233,11 +217,11 @@ export const UnboundedDropLastSequence = defineSequence({
         return this.source.popFront();
     },
     back: function(){
-        this.initializeBack();
+        if(!this.initializedBack) this.initializeBack();
         return this.source.back();
     },
     popBack: function(){
-        this.initializeBack();
+        if(!this.initializedBack) this.initializeBack();
         return this.source.popBack();
     },
     index: function(i){
@@ -247,18 +231,9 @@ export const UnboundedDropLastSequence = defineSequence({
         return this.source.slice(i, j);
     },
     copy: function(){
-        const copy = new UnboundedDropLastSequence(
-            this.dropTarget, this.source.copy()
+        return new UnboundedDropLastSequence(
+            this.dropTarget, this.source.copy(), this.initializedBack
         );
-        copy.back = this.back;
-        copy.popBack = this.popBack;
-        return copy;
-    },
-    reset: function(){
-        this.source.reset();
-        delete this.back;
-        delete this.popBack;
-        return this;
     },
     rebase: function(source){
         this.source = source;
@@ -292,15 +267,18 @@ export const UnboundedDropLastPredicateSequence = defineSequence({
         hi => new UnboundedDropLastPredicateSequence(9, i => i === "l", hi.repeat("hello")),
     ],
     constructor: function UnboundedDropLastPredicateSequence(
-        dropTarget, predicate, source, droppedElements = undefined
+        dropTarget, predicate, source, droppedElements = undefined,
+        initializedBack = undefined
     ){
         this.dropTarget = dropTarget;
         this.predicate = predicate;
         this.source = source;
         this.droppedElements = droppedElements || 0;
+        this.initializedBack = initializedBack;
         this.maskAbsentMethods(source);
     },
     initializeBack: function(){
+        this.initializedBack = true;
         while(
             this.droppedElements < this.dropTarget &&
             this.predicate(this.source.back())
@@ -308,19 +286,6 @@ export const UnboundedDropLastPredicateSequence = defineSequence({
             this.source.popBack();
             this.droppedElements++;
         }
-        this.back = function(){
-            return this.source.back();
-        };
-        this.popBack = function(){
-            this.source.popBack();
-            while(
-                this.droppedElements < this.dropTarget &&
-                this.predicate(this.source.back())
-            ){
-                this.source.popBack();
-                this.droppedElements++;
-            }
-        };
     },
     bounded: () => false,
     unbounded: () => true,
@@ -332,12 +297,19 @@ export const UnboundedDropLastPredicateSequence = defineSequence({
         return this.source.popFront();
     },
     back: function(){
-        this.initializeBack();
+        if(!this.initializedBack) this.initializeBack();
         return this.source.back();
     },
     popBack: function(){
-        this.initializeBack();
-        return this.source.popBack();
+        if(!this.initializedBack) this.initializeBack();
+        this.source.popBack();
+        while(
+            this.droppedElements < this.dropTarget &&
+            this.predicate(this.source.back())
+        ){
+            this.source.popBack();
+            this.droppedElements++;
+        }
     },
     index: function(i){
         return this.source.index(i);
@@ -346,18 +318,10 @@ export const UnboundedDropLastPredicateSequence = defineSequence({
         return this.source.slice(i, j);
     },
     copy: function(){
-        const copy = new UnboundedDropLastPredicateSequence(
-            this.dropTarget, this.predicate, this.source.copy(), this.droppedElements
+        return new UnboundedDropLastPredicateSequence(
+            this.dropTarget, this.predicate, this.source.copy(),
+            this.droppedElements, this.initializedBack
         );
-        copy.back = this.back;
-        copy.popBack = this.popBack;
-        return copy;
-    },
-    reset: function(){
-        this.source.reset();
-        delete this.back;
-        delete this.popBack;
-        return this;
     },
     rebase: function(source){
         this.source = source;
@@ -416,7 +380,7 @@ export const dropLast = wrap({
                 new EmptySequence()
             );
         }else if(predicate){
-            if(source.length && source.length() <= dropTarget){
+            if(source.nativeLength && source.length() <= dropTarget){
                 return new FilterSequence(element => !predicate(element), source);
             }else if(source.unbounded()){
                 if(source.back){
@@ -427,12 +391,12 @@ export const dropLast = wrap({
             }else{
                 return new DropLastPredicateSequence(drop, predicate, source);
             }
-        }else if(source.length){
+        }else if(source.nativeLength){
             const sourceLength = source.length();
             return (sourceLength <= drop ?
                 new EmptySequence() :
-                (source.slice ?
-                    source.slice(0, sourceLength - drop) :
+                (source.nativeSlice ?
+                    source.nativeSlice(0, sourceLength - drop) :
                     new HeadSequence(sourceLength - drop, source)
                 )
             );
