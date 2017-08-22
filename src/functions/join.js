bdecd4eee12a29ba6779a03c28c979f1af4a4a85
Sophie Kirschner <sophie@thehumangeo.com>
2017-08-22 08:58:39 +0300

    Rewrite join.js to be less bad

--- src/functions/join.js
+++ src/functions/join.js
@@ -3,177 +3,70 @@ import {defineSequence} from "../core/defineSequence";
 import {wrap} from "../core/wrap";
 
 import {copyable} from "./copyable";
-import {ForwardFlattenSequence} from "./flatten";
+import {FlattenSequence} from "./flatten";
 
-export const ForwardJoinSequence = defineSequence({
-    constructor: function ForwardJoinSequence(
+export const JoinSequence = defineSequence({
+    constructor: function JoinSequence(
         source, delimiter, frontSource = undefined,
-        frontDelimiter = undefined, onDelimiter = undefined
+        onDelimiter = undefined, initializedFront = undefined
     ){
         this.source = source;
         this.delimiter = delimiter;
         this.frontSource = frontSource;
-        this.frontDelimiter = frontDelimiter;
         this.onDelimiter = onDelimiter;
+        this.initializedFront = initializedFront;
     },
-    reverse: function(){
-        return new BackwardJoinSequence(this.source, this.delimiter);
-    },
-    initialize: function(){
+    initializeFront: function(){
+        this.initializedFront = true;
         if(this.source.done()){
-            this.front = () => undefined;
-            this.popFront = () => {};
+            this.frontSource = this.source;
         }else if(this.delimiter.done()){
             this.frontSource = asSequence(this.source.nextFront());
-            while(this.frontSource.done() && !this.source.done()){
+            while((!this.frontSource || this.frontSource.done()) && !this.source.done()){
                 this.frontSource = asSequence(this.source.nextFront());
             }
-            this.done = function(){
-                return this.source.done() && this.frontSource.done();
-            };
-            this.front = function(){
-                return this.frontSource.front();
-            };
-            this.popFront = function(){
-                this.frontSource.popFront();
-                while(this.frontSource.done() && !this.source.done()){
-                    this.frontSource = asSequence(this.source.nextFront());
-                }
-            };
         }else{
             this.frontDelimiter = this.delimiter.copy();
             this.frontSource = asSequence(this.source.nextFront());
             this.onDelimiter = this.frontSource.done();
-            this.done = function(){
-                return this.source.done() && this.frontSource.done();
-            };
-            this.front = function(){
-                return (this.onDelimiter ?
-                    this.frontDelimiter.front() : this.frontSource.front()
-                );
-            };
-            this.popFront = function(){
-                if(this.onDelimiter){
-                    this.frontDelimiter.popFront();
-                    if(this.frontDelimiter.done()){
-                        this.frontDelimiter = this.delimiter.copy();
-                        this.onDelimiter = false;
-                    }
-                }else if(!this.frontSource.done()){
-                    this.frontSource.popFront();
-                    if(this.frontSource.done() && !this.source.done()){
-                        this.frontSource = asSequence(this.source.nextFront());
-                        this.onDelimiter = true;
-                    }
-                }else if(!this.source.done()){
-                    this.frontSource = asSequence(this.source.nextFront());
-                    this.onDelimiter = true;
-                }
-            };
         }
     },
     bounded: () => false,
-    unbounded: () => false,
+    unbounded: function(){
+        return this.source.unbounded();
+    },
     done: function(){
-        return this.source.done();
-    },
-    length: null,
-    left: null,
-    front: function(){
-        this.initialize();
-        return this.front();
-    },
-    popFront: function(){
-        this.initialize();
-        return this.popFront();
-    },
-    rebase: function(source){
-        this.source = source;
-        return this;
-    },
-});
-
-export const BackwardJoinSequence = defineSequence({
-    constructor: function BackwardJoinSequence(
-        source, delimiter, frontSource = undefined,
-        frontDelimiter = undefined, onDelimiter = undefined
-    ){
-        this.source = source;
-        this.delimiter = delimiter;
-        this.frontSource = frontSource;
-        this.frontDelimiter = frontDelimiter;
-        this.onDelimiter = onDelimiter;
-    },
-    reverse: function(){
-        return new ForwardJoinSequence(this.source, this.delimiter);
-    },
-    initialize: function(){
-        if(this.source.done()){
-            this.front = () => undefined;
-            this.popFront = () => {};
-        }else if(this.delimiter.done()){
-            this.frontSource = asSequence(this.source.nextBack());
-            while(this.frontSource.done() && !this.source.done()){
-                this.frontSource = asSequence(this.source.nextBack());
-            }
-            this.done = function(){
-                return this.source.done() && this.frontSource.done();
-            };
-            this.front = function(){
-                return this.frontSource.back();
-            };
-            this.popFront = function(){
-                this.frontSource.popBack();
-                while(this.frontSource.done() && !this.source.done()){
-                    this.frontSource = asSequence(this.source.nextBack());
-                }
-            };
+        if(this.initializedFront){
+            return this.source.done() && this.frontSource.done();
         }else{
-            this.frontDelimiter = this.delimiter.copy();
-            this.frontSource = asSequence(this.source.nextBack());
-            this.onDelimiter = this.frontSource.done();
-            this.done = function(){
-                return this.source.done() && this.frontSource.done();
-            };
-            this.front = function(){
-                return (this.onDelimiter ?
-                    this.frontDelimiter.back() : this.frontSource.back()
-                );
-            };
-            this.popFront = function(){
-                if(this.onDelimiter){
-                    this.frontDelimiter.popBack();
-                    if(this.frontDelimiter.done()){
-                        this.frontDelimiter = this.delimiter.copy();
-                        this.onDelimiter = false;
-                    }
-                }else if(!this.frontSource.done()){
-                    this.frontSource.popBack();
-                    if(this.frontSource.done() && !this.source.done()){
-                        this.frontSource = asSequence(this.source.nextBack());
-                        this.onDelimiter = true;
-                    }
-                }else if(!this.source.done()){
-                    this.frontSource = asSequence(this.source.nextBack());
-                    this.onDelimiter = true;
-                }
-            };
+            return this.source.done() && this.delimiter.done();
         }
     },
-    bounded: () => false,
-    unbounded: () => false,
-    done: function(){
-        return this.source.done();
-    },
-    length: null,
-    left: null,
     front: function(){
-        this.initialize();
-        return this.front();
+        if(!this.initializedFront) this.initializeFront();
+        return this.frontSource.front();
     },
     popFront: function(){
-        this.initialize();
-        return this.popFront();
+        if(!this.initializedFront) this.initializeFront();
+        this.frontSource.popFront();
+        if(this.frontSource.done()){
+            if(this.onDelimiter){
+                this.frontSource = asSequence(this.source.nextFront());
+                if(!this.frontSource || this.frontSource.done()){
+                    this.onDelimiter = true;
+                    this.frontSource = this.delimiter.copy();
+                }else{
+                    this.onDelimiter = false;
+                }
+            }else if(this.delimiter.done()){
+                while((!this.frontSource || this.frontSource.done()) && !this.source.done()){
+                    this.frontSource = asSequence(this.source.nextFront());
+                }
+            }else if(!this.source.done()){
+                this.frontSource = this.delimiter.copy();
+                this.onDelimiter = true;
+            }
+        }
     },
     rebase: function(source){
         this.source = source;
@@ -185,21 +78,25 @@ export const join = wrap({
     name: "join",
     attachSequence: true,
     async: false,
-    sequences: [
-        ForwardJoinSequence,
-        BackwardJoinSequence
-    ],
     arguments: {
-        ordered: [wrap.expecting.sequence, wrap.expecting.sequence]
+        ordered: [
+            wrap.expecting.sequence,
+            wrap.expecting.optional(wrap.expecting.sequence)
+        ],
     },
     implementation: (source, delimiter) => {
-        if(delimiter && !delimiter.done()){
-            return new ForwardJoinSequence(source, copyable(delimiter));
+        if(delimiter){
+            return new JoinSequence(source, copyable(delimiter));
         }else{
             // Optimized implementation for when there is no delimiter
-            // or when the delimiter is an empty sequence.
-            return new ForwardFlattenSequence(source);
+            return new FlattenSequence(source);
         }
+    },
+    tests: process.env.NODE_ENV !== "development" ? undefined : {
+        "basicUsage": hi => {
+            const strings = ["one", "two", "three"];
+            hi.assertEqual(hi(strings).join(", "), "one, two, three");
+        },
     },
 });
 
